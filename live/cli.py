cefc74791036bb7c23fa8f5193607fb4b6803771
cupcakearmy <nicco.borgioli@gmail.com>
2019-10-22 12:27:15 +0200

    live code to show

---
+++ live/cli.py
@@ -0,0 +1,13 @@
+print('Shalom')
+
+animals = ['elephant', 'cat', 'unicorn']
+for animal in animals:
+    print(animal)
+
+
+def hello(name):
+    print('Hello ' + name + ' 👋')
+
+
+hello('Tom')
+hello('Jerry')
