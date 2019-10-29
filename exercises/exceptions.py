304ed3ddd5b8579bfe8b5b2b8d63b04f835f9c96
Nick Lehmann <nick@lehmann.sh>
2019-10-29 14:26:14 +0100

    Add exercise about custom exceptions

--- exercises/exceptions.py
+++ exercises/exceptions.py
@@ -1,6 +1,5 @@
 import sys
 import unittest
-import tempfile
 
 
 def divide(dividend: int, divisor: int) -> float:
@@ -13,7 +12,7 @@ def divide(dividend: int, divisor: int) -> float:
     pass
 
 
-def count_lines_of_files(filename) -> int:
+def count_lines_of_files(filename: str) -> int:
     """
     Return the number of lines in the file with the
     given filename.
@@ -22,6 +21,19 @@ def count_lines_of_files(filename) -> int:
 
     Note: Use the builtin open() function and the associated readlines()
     function to read the lines from the file.
+    """
+    pass
+
+
+def sell_vodka(money: int, age: int):
+    """
+    Try to sell vodka to a person with the given age.
+
+    If the person is underage, throw a PersonTooYoungException.
+    If the person does not have enough money, throw an NotEnoughMoneyException.
+
+    The price for one bottle of vodka is 10. Return true if selling was
+    successful.
     """
     pass
 
@@ -37,6 +49,18 @@ class ExceptionTestCase(unittest.TestCase):
     def test_count_lines_of_code(self):
         self.assertEqual(count_lines_of_files('./dummy.py'), 6)
         self.assertEqual(count_lines_of_files('./does_not_exist.py'), 0)
+
+    def test_sell_vodka(self):
+        self.assertTrue(sell_vodka(100, 40))
+        self.assertTrue(sell_vodka(10, 18))
+
+        with self.assertRaises(Exception) as context:
+            sell_vodka(100, 5)
+        self.assertEqual(type(context.exception).__name__, 'PersonTooYoungException')
+
+        with self.assertRaises(Exception) as context:
+            sell_vodka(5, 20)
+        self.assertEqual(type(context.exception).__name__, 'NotEnoughMoneyException')
 
 
 if len(sys.argv) > 1 and sys.argv[1] == 'test':
