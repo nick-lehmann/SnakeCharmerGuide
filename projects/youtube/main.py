7b3f22faf90f8172c76388ee1c66539ad944a387
cupcakearmy <nicco.borgioli@gmail.com>
2019-11-19 14:31:54 +0100

    gitignore

--- projects/youtube/main.py
+++ projects/youtube/main.py
@@ -1,13 +1,10 @@
-from pytube import YouTube as yt
+from pytube import YouTube as YT
 
 
 def show_progress_bar(stream, chunk, file_handle, bytes_remaining):
     print(bytes_remaining)
 
 
-yt.register_on_progress_callback(show_progress_bar)
-
-print(
-    yt('https://www.youtube.com/watch?v=v2xf9KOQVkM')
-    .streams.get_by_itag(140).download()
-)
+yt = YT('https://www.youtube.com/watch?v=v2xf9KOQVkM')
+yt.register_on_progress_callback(func=show_progress_bar)
+yt.streams.get_by_itag(140).download()
