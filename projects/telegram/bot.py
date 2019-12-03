9fc925f1c9f2a9efabd337738e6d293b0bd14885
cupcakearmy <nicco.borgioli@gmail.com>
2019-12-03 13:12:04 +0100

    python bot

--- projects/telegram/bot.py
+++ projects/telegram/bot.py
@@ -1,38 +1,52 @@
 import os
 from datetime import date
+from typing import List, Dict, Any
 
 import requests
 from bs4 import BeautifulSoup
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
 
-TOKEN = os.getenv('BOT_TOKEN')
-# TOKEN = ''
+TOKEN = ''
 
 
-def get_lunch(day: int) -> list:
-    """
-    Fetch available dishes online.
-    """
-    url = 'https://www.studentenwerk-dresden.de/mensen/speiseplan/w0-d{}.html?view=list'.format(day)
-    print('Fetching lunch from: {}'.format(url))
+def get_mensa_names() -> List[str]:
+    return list(get_all_mensa().keys())
+
+
+def get_all_mensa() -> Dict[str, Any]:
+    url = 'https://www.studentenwerk-dresden.de/mensen/speiseplan/?view=list'
 
     content_raw = requests.get(url).content
     content = BeautifulSoup(content_raw, 'html.parser')
+    cards = content.select('.card')
 
-    alte_mensa_card = content.select('.card')[5]
-    menu_items = alte_mensa_card.select('a.text-dark')
+    found = {}
+    for card in cards:
+        headers = card.select('h3.card-header')
+        if not headers:
+            continue
 
+        header = headers[0].text
+        found[header] = card
+
+    return found
+
+
+def get_dishes_for_card(card):
+
+    menu_items = card.select('li.swdd-link-list-item')
     dishes = list()
     for index, menu_item in enumerate(menu_items):
 
         dish_name = menu_item.select('span')[1].text
         dish_price = menu_item.select('strong')[0].text
-        print('Dish {}'.format(index))
-        print('- {}'.format(dish_name))
+        print(f'Dish {index}')
+        print(f'- {dish_name}')
         if dish_price:
             dish_price = dish_price.split('/')[0].strip().replace(' ', '')
-            print('=> {}'.format(dish_price))
+            print(f'=> {dish_price}')
 
         dishes.append({
             'name': dish_name,
@@ -42,35 +56,40 @@ def get_lunch(day: int) -> list:
     return dishes
 
 
-def lunch_action(update, context):
-    """
-    Send back available dishes to the user.
-    """
-    print('Get lunch dishes')
+def get_lunch(name: str) -> list:
+    mensas = get_all_mensa()
+    return get_dishes_for_card(mensas[name])
 
-    today = date.today()
-    current_weekday = (today.weekday() + 1) % 6
-    print('Current weekday is {}'.format(current_weekday))
 
-    dishes = get_lunch(current_weekday)
+def list_action(update, context):
+    names = get_mensa_names()
+    keyboard = [
+        [InlineKeyboardButton(name, callback_data=name)]
+        for name in names
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('🤔 Choose a mensa ⬇', reply_markup=reply_markup)
 
-    if not dishes:
-        update.message.reply_text('No lunch today')
-        return
 
-    update.message.reply_text('Lunch on {}'.format(today.strftime('%d.%m.%y')))
+def list_callback(update, context):
+    query = update.callback_query
+    selected = query.data
+
+    query.edit_message_text(text=f'Dishes for {selected}')
+
+    dishes = get_lunch(selected)
     for dish in dishes:
-        update.message.reply_text('{} {}'.format(dish['name'], dish['price']))
+        query.message.reply_text(f"{dish['name']} {dish['price']}")
 
 
 def start_bot():
     updater = Updater(TOKEN, use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler('lunch', lunch_action))
+    updater.dispatcher.add_handler(CommandHandler('list', list_action))
+    updater.dispatcher.add_handler(CallbackQueryHandler(list_callback))
 
     updater.start_polling()
     updater.idle()
 
 
 start_bot()
-
